0329acbdf1afbc9c68ac3b86a8c8ec66a225ca0f
quaei676 <quaei676@gmail.com>
2018-07-06 17:39:46 +0800

    test

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yo1/minicarcy/src
-# Build directory: /home/yo1/minicarcy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(minicar)
-subdirs(minicarcy)
--- build/minicarcy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yo1/minicarcy/src/minicarcy
-# Build directory: /home/yo1/minicarcy/build/minicarcy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
